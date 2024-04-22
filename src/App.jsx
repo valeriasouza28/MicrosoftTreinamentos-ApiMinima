39bfd0f3ff2846821ddf78752997edf8a4add2ff
valeriasouza28 <valeriasouza28@gmail.com>
2024-04-22 09:21:46 -0300

    código para simulação de api

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Pizza from "./Pizza";
+
+function App() {
+    // const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Pizza />
+        </>
+    );
+}
+
+export default App;
